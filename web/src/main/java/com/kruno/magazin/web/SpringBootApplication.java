57ad4bac7143dc34a4c3a3027c429c8f84006d8f
Krunoslav Magazin <krunoslav.magazin@solutionfactory.ch>
2017-06-07 00:12:41 +0200

    Fix in configuration, app works on wildfly.

--- web/src/main/java/com/kruno/magazin/web/SpringBootApplication.java
+++ web/src/main/java/com/kruno/magazin/web/SpringBootApplication.java
@@ -1,7 +1,11 @@
 package com.kruno.magazin.web;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 // needed for running on the wildfly server
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,10 +13,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableAutoConfiguration
-@ComponentScan({"com.kruno.magazin"})
-//@EnableJpaRepositories({"com.kruno.magazin"})
-//@EntityScan({"com.kruno.magazin"})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ComponentScan({"hr.kruno.magazin"})
+// XXX or annotation ?
+//@SpringBootApplication
 public class SpringBootApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootApplication.class);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
 }
